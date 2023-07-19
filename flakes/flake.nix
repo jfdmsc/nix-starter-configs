b069d4fb34948ad98bd71a233cb31dcd7f5b2042
jfdmsc <105931223+jfdmsc@users.noreply.github.com>
2023-07-19 00:57:15 -0400

    fix flake.nix

--- flakes/flake.nix
+++ flakes/flake.nix
@@ -7,7 +7,7 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
   
-  outputs = {self, nixpkgs, home-manager }: {
+  outputs = {self, nixpkgs, home-manager, ...}: {
     nixosConfigurations = {
     avell-nixos = nixpkgs.lib.nixosSystem {
      system = "x86_64-linux";
