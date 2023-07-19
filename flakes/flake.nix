c81b078852075fb7a1e9852fb9c3caf38eddd198
jfdmsc <105931223+jfdmsc@users.noreply.github.com>
2023-07-18 23:54:09 -0400

    fix typo

--- flakes/flake.nix
+++ flakes/flake.nix
@@ -12,7 +12,7 @@
     avell-nixos = nixpkgs.lib.nixosSystem {
      system = "x86_64-linux";
      modules = [
-     ./nixos/configurations.nix
+     ./nixos/configuration.nix
      home-manager.nixosModules.home-manager {
        home-manager.useGlobalPkgs = true;
        home-manager.useUserPackages = true;
